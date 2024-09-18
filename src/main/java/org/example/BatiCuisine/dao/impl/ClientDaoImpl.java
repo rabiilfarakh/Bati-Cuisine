6b0283f167990a3f5aea6133b002abff691b1e48
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-09-18 12:19:24 +0100

    BAT-24 <realiser le diagramme de class>

--- src/main/java/org/example/BatiCuisine/dao/impl/ClientDaoImpl.java
+++ src/main/java/org/example/BatiCuisine/dao/impl/ClientDaoImpl.java
@@ -1,6 +1,26 @@
 package org.example.BatiCuisine.dao.impl;
 
 import org.example.BatiCuisine.dao.inter.ClientDao;
+import org.example.BatiCuisine.entities.Client;
+
+import java.util.Optional;
 
 public class ClientDaoImpl implements ClientDao {
+
+
+
+    @Override
+    public void ajouterClient(Client client) {
+
+    }
+
+    @Override
+    public Optional<Client> afficherInformations(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void appliquerRemise() {
+
+    }
 }
--- src/main/java/org/example/BatiCuisine/dao/inter/ClientDao.java
+++ src/main/java/org/example/BatiCuisine/dao/inter/ClientDao.java
@@ -1,4 +1,12 @@
 package org.example.BatiCuisine.dao.inter;
 
+import org.example.BatiCuisine.entities.Client;
+
+import java.util.Optional;
+
 public interface ClientDao {
+
+    void ajouterClient(Client client);
+    Optional<Client> afficherInformations(Integer id);
+    void appliquerRemise();
 }
