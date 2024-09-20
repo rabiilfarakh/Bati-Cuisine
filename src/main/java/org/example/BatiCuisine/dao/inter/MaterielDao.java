06f338ab8474b8cb3eb22af1505d36c46c6ad524
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-09-20 11:58:30 +0100

    ajouter l'interface MaterielDao avec les methodes -void ajouterMateriel(Materiel materiel, Integer projetId); -List<Materiel> afficherMaterielParProjet(Integer projetId); -void modifierMateriel(Materiel materiel); -void supprimerMateriel(Integer id);

--- src/main/java/org/example/BatiCuisine/dao/inter/MaterielDao.java
+++ src/main/java/org/example/BatiCuisine/dao/inter/MaterielDao.java
@@ -1,4 +1,12 @@
 package org.example.BatiCuisine.dao.inter;
 
+import org.example.BatiCuisine.entities.Materiel;
+
+import java.util.List;
+
 public interface MaterielDao {
+    void ajouterMateriel(Materiel materiel, Integer projetId);
+    List<Materiel> afficherMaterielParProjet(Integer projetId);
+    void modifierMateriel(Materiel materiel);
+    void supprimerMateriel(Integer id);
 }
