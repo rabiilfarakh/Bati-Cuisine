4e9d26ef9114e1b7619c1cc3efb51b53e515ccfe
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-09-20 12:18:57 +0100

    ajouter l'interface ProjetDao - void ajouterProjet(Projet projet, Integer clientId); - Projet afficherProjet(Integer id); - void supprimerProjet(Integer id); - List<Projet> listerProjetsParClient(Integer clientId);

--- src/main/java/org/example/BatiCuisine/dao/inter/ProjetDao.java
+++ src/main/java/org/example/BatiCuisine/dao/inter/ProjetDao.java
@@ -1,4 +1,12 @@
 package org.example.BatiCuisine.dao.inter;
 
+import org.example.BatiCuisine.entities.Projet;
+
+import java.util.List;
+
 public interface ProjetDao {
+    void ajouterProjet(Projet projet, Integer clientId);
+    Projet afficherProjet(Integer id);
+    void supprimerProjet(Integer id);
+    List<Projet> listerProjetsParClient(Integer clientId);
 }
